0775bc7544865f9b3044906e59ebb16dc88224d6
Madhuri140492 <madhurideshmukh1404@gmail.com>
2023-01-12 14:41:50 +0530

    [Added :check equality of two lines]

--- LineComprison.java
+++ LineComprison.java
@@ -1,37 +1,30 @@
-//package Line_Comparison_Problem;
+package com.bridgelabz.linecomparision;
+
 import java.util.Scanner;
-/*public class LineComprison {
 
+/*As a fan of geometry, I want to check equality of two lines based on the end points, So
+that I know when two lines are the equal.
+- Using Java equals method to check equality of 2 Lengths is preferable.*/
+
+public class EqualityOfLine {
     public static void main(String[] args) {
-        System.out.print("Welcome to the Line Comparison Problem");
-    }
-*/
-//package com.bridgelabz.linecomparision;
-
-
-/*As a fan of geometry, I want to model a line based on a point
-consisting of (x, y) co-ordinates using the Cartesian system,So that I can calculate its length.
-- A Length as 2 Points (x1, y1) and (x2, y2)
-- Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2*/
-//public class CalculateLength {
-public class LineComprison {
-    public static void main(String[] args) {
-        Scanner i = new Scanner(System.in);
-
-        System.out.println("Enter first co-ordinates x1 and Y1");
-
-        double x1 = i.nextDouble();
-        double y1 = i.nextDouble();
-
-        System.out.println("Enter second co-ordinates x2 and y2");
-        double x2 = i.nextDouble();
-        double y2 = i.nextDouble();
-        System.out.println("Length of a line is " + LineComprison.lengthOfLine(x1, x2, y1, y2));
-        i.close();
-    }
-
-    public static double lengthOfLine(double x1, double x2, double y1, double y2) {
-        double form = Math.sqrt((Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2));
-        return form;
+        System.out.println("Enter the co-ordinates for line 1");
+        Scanner sc = new Scanner(System.in);
+        double a, b, c, d, e, f, g, h;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        double line_1 = CalculateLength.lengthOfLine(a, b, c, d);
+        System.out.println("Enter the co-ordinates for line 2");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        d = sc.nextInt();
+        double line_2 = CalculateLength.lengthOfLine(a, b, c, d);
+        String s1 = line_1 + "";
+        String s2 = Double.toString(line_2);
+        System.out.println(s1 + "\t" +s2);
+        System.out.println(s1.equals(s2));
     }
 }
