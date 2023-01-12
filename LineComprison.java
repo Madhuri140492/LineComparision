f2115675aecff9d26887e7d0f83331f9d44fb524
Madhuri140492 <madhurideshmukh1404@gmail.com>
2023-01-12 14:46:02 +0530

    [Added : Using Java compareTo method to compare 2 Lengths]

--- LineComprison.java
+++ LineComprison.java
@@ -2,15 +2,15 @@ package com.bridgelabz.linecomparision;
 
 import java.util.Scanner;
 
-/*As a fan of geometry, I want to check equality of two lines based on the end points, So
-that I know when two lines are the equal.
-- Using Java equals method to check equality of 2 Lengths is preferable.*/
+/*As a fan of geometry, I want to compare two lines based on the end points, So that I know
+one line is equal, greater or less than the other line.
+- Using Java compareTo method to compare 2 Lengths is preferable.*/
 
-public class EqualityOfLine {
+public class Lexographically {
     public static void main(String[] args) {
         System.out.println("Enter the co-ordinates for line 1");
         Scanner sc = new Scanner(System.in);
-        double a, b, c, d, e, f, g, h;
+        int a, b, c, d, e, f, g, h;
         a = sc.nextInt();
         b = sc.nextInt();
         c = sc.nextInt();
@@ -25,6 +25,7 @@ public class EqualityOfLine {
         String s1 = line_1 + "";
         String s2 = Double.toString(line_2);
         System.out.println(s1 + "\t" +s2);
-        System.out.println(s1.equals(s2));
+        System.out.println(s1.compareTo(s2));
+
     }
 }
